318d4c1e137079834e96149afe18bd42a73b6d78
supershal <patel.shalin@gmail.com>
2020-08-16 01:37:59 -0700

    add performance test

--- testing/performance_test.go
+++ testing/performance_test.go
@@ -1,21 +1,44 @@
 package testing
 
 import (
+	"fmt"
 	"log"
 	"sync"
+	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/supershal/mygraph/graph"
 )
 
-var perfServerAddr string = "localhost:8082"
+func TestAddGraph_1000(t *testing.T) {
+	g := SampleGraph()
+	ids := sendGraphParallel(g, 1000)
+	assert.Equal(t, len(ids), 1000, "Graph service should return 1000 graphs")
+	graphs := map[int64]bool{}
+	for _, id := range ids {
+		graphs[id] = true
+	}
+	for i := 0; i < 1000; i++ {
+		_, ok := graphs[int64(i)]
+		assert.Equal(t, ok, true, fmt.Sprintf("Graph with id %v should be returned", i))
+	}
+}
 
-func sendGraphParallel(g *graph.Undirected, times int) {
+func TestDeleteGraph_1000(t *testing.T) {
+	g := SampleGraph()
+	ids := sendGraphParallel(g, 1000)
+	assert.Equal(t, len(ids), 1000, "Graph service should return 1000 graphs")
+	deleteGraphParallel(ids)
+}
+func sendGraphParallel(g *graph.Undirected, times int) []int64 {
 	var wg sync.WaitGroup
+	var m sync.Mutex
+	graphIds := make([]int64, 0)
 
 	sendGraph := func() int64 {
 		defer wg.Done()
 		// Create new connection for each operation
-		conn, client := NewClientConnection(perfServerAddr)
+		conn, client := NewClientConnection(funcServerAddr)
 		defer conn.Close()
 		var id int64
 		var err error
@@ -24,6 +47,9 @@ func sendGraphParallel(g *graph.Undirected, times int) {
 			log.Fatalln(err)
 		}
 		log.Println("graph generated:", id)
+		m.Lock()
+		graphIds = append(graphIds, id)
+		m.Unlock()
 		return id
 	}
 	for i := 0; i < times; i++ {
@@ -31,16 +57,17 @@ func sendGraphParallel(g *graph.Undirected, times int) {
 		go sendGraph()
 	}
 	wg.Wait()
+	return graphIds
 }
 
-func deleteGraphParallel(times int) {
+func deleteGraphParallel(ids []int64) {
 	var wg sync.WaitGroup
 
 	deleteGraph := func(id int64) {
 		defer wg.Done()
 
 		// Create new connection for each operation
-		conn, client := NewClientConnection(perfServerAddr)
+		conn, client := NewClientConnection(funcServerAddr)
 		defer conn.Close()
 		var err error
 		if err = DeleteGraph(client, id); err != nil {
@@ -48,9 +75,9 @@ func deleteGraphParallel(times int) {
 		}
 		log.Println("graph Deleted:", id)
 	}
-	for i := 0; i < times; i++ {
+	for _, id := range ids {
 		wg.Add(1)
-		go deleteGraph(int64(i))
+		go deleteGraph(id)
 	}
 	wg.Wait()
 }
